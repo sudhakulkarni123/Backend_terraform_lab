f2399fe5b139bdb988bc3d562a48ec43c4ca2fb8
sudhakulkarni123 <sudha.kulkarni@cloudreach.com>
2022-07-18 19:06:52 +0530

    adding dynamodb for table

--- backend.tf
+++ backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "talent-academy-sudha-lab-tfstates"
     key = "talent-academy/backend/terraform.tfstates"
     region = "eu-west-1"
+    dynamodb_table = "terraform-lock"
   }
 }
